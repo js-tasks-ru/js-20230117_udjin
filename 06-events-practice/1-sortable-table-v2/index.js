f9d7577d171b4dcb413e6e1327697372ce3d16e1
Евгений <jenek95@mail.ru>
2023-02-15 09:42:21 +0300

    event

--- 06-events-practice/1-sortable-table-v2/index.js
+++ 06-events-practice/1-sortable-table-v2/index.js
@@ -3,19 +3,54 @@ export default class SortableTable {
   element; // Не зависят от входных параметров, то же самое this.element
   subElements = {};
 
-  constructor(headerConfig = [], data = []) {
+  constructor(headerConfig = [], {
+    data = [],
+    sorted = {
+      id: headerConfig.find(item => item.sortable).id,
+      order: 'asc'
+    }
+  } = {}) {
+
     this.headerConfig = headerConfig;
-    this.data = data.data;
+    this.data = data;
     this.isSortLocally = true;
+    this.sorted = sorted;
 
     this.render();
   }
 
-  get template() {
+  onSortClick = event => {
+    const column = event.target.closest('[data-sortable="true"]');
 
+    const toggleOrder = order => {
+      const orders = {
+        asc: 'desc',
+        desc: 'asc',
+      };
+
+      return orders[order];
+    };
+
+    if (column) {
+      const { id, order } = column.dataset; // Из элемента берем текущий data-id, data-order
+      const newOrder = toggleOrder(order);
+      const sortedData = this.sortData(id, newOrder);
+      const arrow = column.querySelector('.sortable-table__sort-arrow'); // Ищем стрелку (<div>)
+
+      column.dataset.order = newOrder; // задаем data-order = newOrder; (asc, desc)
+
+      if (!arrow) {
+        column.append(this.subElements.arrow);// Если нет стрелки, добавляем. append "забирает" стрелку с другого элемента
+      }
+
+      this.subElements.body.innerHTML = this.getTableRows(sortedData);
+    }
   }
 
+  addListners() {
+    this.subElements.header.addEventListener('pointerdown', this.onSortClick);
 
+  }
 
   getTableRow(item) {
     return this.headerConfig.map(({ id, template }) => {
@@ -23,8 +58,7 @@ export default class SortableTable {
     }).join('');
   }
 
-  getTableRows(data = []) {
-    // console.log(data)
+  getTableRows(data) {
     return data.map((item) => {
       return `<a href="/products/${item.id}" class="sortable-table__row">
               ${this.getTableRow(item)}
@@ -35,49 +69,55 @@ export default class SortableTable {
   getHeaderRow(options) {
     const { id, title, sortable } = options;
 
-    return `<div class="sortable-table__cell" data-id="${id}" data-sortable="${sortable}" data-order="">
+    const order = this.sorted.id = id ? this.sorted.order : 'asc';
+
+    return `<div class="sortable-table__cell" data-id="${id}" data-sortable="${sortable}" data-order="${order}">
               <span>${title}</span>
-              <span data-element="arrow" class="sortable-table__sort-arrow">
-                <span class="sort-arrow"></span>
-              </span>
+              ${this.getHeaderSortingArrow(id)}
             </div>`;
   }
 
-  // addMouseEvent(cells) {
-  //   cells.forEach((cell) => cell.addEventListener('click', () => console.log('click')));
-  // } ${this.addMouseEvent(sortableCell)}
+  getHeaderSortingArrow(id) {
+    const isOrderExist = this.sorted.id === id ? this.sorted.order : '';
+    return isOrderExist
+      ? `<span data-element="arrow" class="sortable-table__sort-arrow">
+          <span class="sort-arrow"></span>
+        </span>`
+      : '';
+  }
 
   getTableHeader() {
     //Создадим новый массив из того что вернет нам стрелочная функция и для каждого обьекта в this.headerConfig, вызовем метод getHeaderRow
     const sortableCell = this.headerConfig.map(item => this.getHeaderRow(item)).join('');
 
-
-    return `<div data-element="header" class="sortable-table__header sortable-table__row" onpointerdown= "alert('fff')">
+    return `<div data-element="header" class="sortable-table__header sortable-table__row">
             ${sortableCell}
-
             </div>`;
   }
 
-  getTableBody() {
+  getTableBody(data) {
     return `<div data-element="body" class="sortable-table__body">
-            ${this.getTableRows(this.data)}
+            ${this.getTableRows(data)}
             </div>`;
   }
 
-  getTable() {
+  getTable(data) {
     //вызываем методы, для получения заголовков таблицы и тела таблицы
     return `<div class="sortable-table">
                 ${this.getTableHeader()}
-                ${this.getTableBody()}
+                ${this.getTableBody(data)}
             </div>`;
   }
 
   getSubElements(element) {
+    // element = <div class = "sortable-table">
+    //              <div data-element="header"></div>
+    //               <div data-element="body"></div>
+    //            </div>
     const result = {};
     const elements = element.querySelectorAll('[data-element]');
-
     for (const subElement of elements) {
-      const name = subElement.dataset.element;
+      const name = subElement.dataset.element; // считываем атрибут data-element
 
       result[name] = subElement;
     }
@@ -85,39 +125,38 @@ export default class SortableTable {
   }
 
   render() {
-
+    const { id, order } = this.sorted;
     const wrap = document.createElement('div');
-    wrap.innerHTML = this.getTable(); // getTable вернет строку, которая преобразуется в innerHTML к DOM элементу
+    const sortedData = this.sortData(id, order); // сразу сортируем
+
+    wrap.innerHTML = this.getTable(sortedData); // getTable вернет строку, которая преобразуется в innerHTML к DOM элементу
+
     const element = wrap.firstElementChild; // достаем сам элемент
-
     this.element = element; //присваивает текущий элемент в this.element
-    // this.sort();
-    this.subElements = this.getSubElements(element); // те элементы, которые нужны позже для обновления
-    this.sort();
+
+    this.subElements = this.getSubElements(element); // те элементы, которые нужны позже для обновления { data: <div></div>}
+
+    this.addListners();
   }
 
-  sort(field = 'price', order = 'asc') {
-    const sortedData = this.sortData(field, order);
-    // console.log(sortedData)
-    const allColumns = this.element.querySelectorAll('.sortable-table__cell[data-id]');
-    const currentColumn = this.element.querySelector(`.sortable-table__cell[data-id="${field}"]`);
+  // sort(field = 'price', order = 'asc') {
+  //   const sortedData = this.sortData(field, order);
+  //   const allColumns = this.element.querySelectorAll('.sortable-table__cell[data-id]');
+  //   const currentColumn = this.element.querySelector(`.sortable-table__cell[data-id="${field}"]`);
 
-    // NOTE: Remove sorting arrow from other columns
-    allColumns.forEach(column => {
-      column.dataset.order = '';
-    });
+  //   // NOTE: Remove sorting arrow from other columns
+  //   allColumns.forEach(column => {
+  //     column.dataset.order = '';
+  //   });
 
-    currentColumn.dataset.order = order;
-
-    // console.log(this.getTableRows(sortedData))
-    this.subElements.body.innerHTML = this.getTableRows(sortedData);
-  }
+  //   currentColumn.dataset.order = order;
+  //   this.subElements.body.innerHTML = this.getTableRows(sortedData);
+  // }
 
   sortData(field, order) {
     const arr = [...this.data];
-    // console.log(arr)
     const column = this.headerConfig.find(item => item.id === field);
-    const { sortType } = column;
+    const { sortType, customSorting } = column;
     const directions = {
       asc: 1,
       desc: -1
@@ -130,6 +169,8 @@ export default class SortableTable {
           return direction * (a[field] - b[field]);
         case 'string':
           return direction * a[field].localeCompare(b[field], ['ru', 'en']);
+        case 'custom':
+          return direction * customSorting(a, b);
         default:
           throw new Error(`Unknown type ${sortType}`);
       }
